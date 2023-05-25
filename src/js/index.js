d79ba58464b4e38ebb4948de2ed50bcb3f427764
Tallys Antonio <tallysantonio@gmail.com>
2023-05-25 13:12:30 -0300

    enviando os aquivos primários

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const input = document.querySelectorAll(".input")
+const botao = document.querySelector(".botao")
+const campoObrigatorio = document.querySelectorAll(".campo-obrigatorio")
+
+input.forEach((input, posicao) => {
+    input.addEventListener("change", function () {
+        if (input.value !== "") {
+            input.classList.add("borda-verde")
+
+            campoObrigatorio[posicao].classList.remove("errado")
+            input.classList.remove("borda-vermelha")
+        } else {
+            input.classList.remove("borda-verde")
+        }
+    })
+})
+
+botao.addEventListener('click', () => {
+    input.forEach((input, posicao) => {
+        if (input.value == "") {
+            campoObrigatorio[posicao].classList.add("errado")
+            input.classList.add("borda-vermelha")
+        } else {
+            campoObrigatorio[posicao].classList.remove("errado")
+            input.classList.remove("borda-vermelha")
+        }
+    })
+})
